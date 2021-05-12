5ca23ff7fe2d85fd7b310ea66a528d079e069df8
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-05-12 16:59:44 +0200

    Workspace class implementation

---
+++ src/sample/workspace/Workspace.java
@@ -0,0 +1,25 @@
+package sample.workspace;
+
+
+public class Workspace {
+	private String title;
+	private int height, width;
+	private WorkspaceContent workspaceContent;
+	
+	public Workspace(String title, int width, int height){
+		this.title = title;
+		this.height = height;
+		this.width = width;
+		this.workspaceContent = new WorkspaceContent(this.width, this.height);
+	}
+	
+	public WorkspaceContent getWorkspaceContent() {
+		return workspaceContent;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+
+}
