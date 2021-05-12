73f6a78d263a358674b7109f2df4b629283fb459
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-05-12 17:06:44 +0200

    Controller events interface implementation

---
+++ src/sample/ControllerEvents.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public interface ControllerEvents {
+	
+	void setColorFirstEvent(ActionEvent event);
+	
+	void setColorSecondaryEvent(ActionEvent event);
+	
+	void addLayerEvent(ActionEvent event);
+	
+	
+}
