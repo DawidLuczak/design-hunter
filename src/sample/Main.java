427d231430de303d1029c338a72a27785a6f0ebe
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-05-12 17:09:52 +0200

    Main class

---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.util.Objects;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
+        primaryStage.setTitle("Design Hunter");
+        primaryStage.setScene(new Scene(root, (int) (Toolkit.getDefaultToolkit().getScreenSize().width / 4 * 3), (int) (Toolkit.getDefaultToolkit().getScreenSize().height / 4 * 3), true, SceneAntialiasing.BALANCED));
+        primaryStage.show();
+        primaryStage.setResizable(true);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
