80532145752b941954a46c887290b74bf7ff2fde
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-05-14 15:23:39 +0200

    Layer class update

--- src/sample/workspace/Layer.java
+++ src/sample/workspace/Layer.java
@@ -1,18 +1,20 @@
 package sample.workspace;
 
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.StackPane;
 
 public class Layer {
 	private String title;
 	private StackPane stackPane;
-	private Content activeContent;
+	private Canvas activeContent;
 	
 	public Layer(String title){
 		this.title = title;
 		this.stackPane = new StackPane();
 	}
 	
-	public void addContentElement(Content content){
+	public void addContentElement(Canvas content){
 		this.stackPane.getChildren().add(content);
 		this.activeContent = content;
 	}
@@ -20,6 +22,15 @@ public class Layer {
 	public void removeContentElement(){
 		if (activeContent != null)
 			stackPane.getChildren().remove(activeContent);
+			
+	}
+	
+	public Canvas getObject(int i){
+		if (stackPane.getChildren().size() > i){
+			return (Canvas) stackPane.getChildren().get(i);
+		} else {
+			return null;
+		}
 	}
 	
 	public String getTitle() {
@@ -31,11 +42,11 @@ public class Layer {
 		return stackPane;
 	}
 	
-	public Content getActiveContent() {
+	public Canvas getActiveContent() {
 		return activeContent;
 	}
 	
-	public void setActiveContent(Content activeContent) {
+	public void setActiveContent(Canvas activeContent) {
 		this.activeContent = activeContent;
 	}
 }
