ed831db888233b1d877055686fdd47cc91a24c99
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-05-14 15:22:20 +0200

    New drawing method available

--- src/sample/workspace/drawingtools/DrawingTools.java
+++ src/sample/workspace/drawingtools/DrawingTools.java
@@ -1,6 +1,9 @@
 package sample.workspace.drawingtools;
 
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import sample.workspace.Workspace;
+import sample.workspace.WorkspaceContent;
 import sample.workspace.drawingtools.shapes.FillType;
 import sample.workspace.drawingtools.shapes.Shapes;
 import static sample.workspace.drawingtools.DrawRectangle.drawRectangle;
@@ -20,6 +23,22 @@ public interface DrawingTools extends DrawCircle, DrawRectangle, DrawTriangle, D
 				drawTriangle(graphicsContext, pointA, pointA.distance(pointB), pointA.angle(pointB), fillType);
 			} else if (shape.equals(Shapes.LINE)) {
 				drawLine(graphicsContext, pointA, pointB);
+			}
+		}
+	}
+	
+	static void draw(WorkspaceContent workspaceContent, Point pointA, Point pointB){
+		Canvas content = workspaceContent.getActiveLayer().getActiveContent();
+		
+		if (workspaceContent.getShape() != null) {
+			if (workspaceContent.getShape().equals(Shapes.RECTANGLE)) {
+				drawRectangle(content.getGraphicsContext2D(), pointA, pointB, workspaceContent.getFillType());
+			} else if (workspaceContent.getShape().equals(Shapes.CIRCLE)){
+				drawEllipse(content.getGraphicsContext2D(), pointA, pointB, workspaceContent.getFillType());
+			} else if (workspaceContent.getShape().equals(Shapes.TRIANGLE)) {
+				drawTriangle(content.getGraphicsContext2D(), pointA, pointA.distance(pointB), pointA.angle(pointB), workspaceContent.getFillType());
+			} else if (workspaceContent.getShape().equals(Shapes.LINE)) {
+				drawLine(content.getGraphicsContext2D(), pointA, pointB);
 			}
 		}
 	}
