59f8c91b3b6c9cd10619ccad21d0df1903aacfa2
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-05-14 15:15:15 +0200

    Set Shape method update

--- src/sample/workspace/drawingtools/shapes/ShapeInterface.java
+++ src/sample/workspace/drawingtools/shapes/ShapeInterface.java
@@ -4,13 +4,18 @@ import sample.Controller;
 
 public interface ShapeInterface {
 	
-	static void setShape(Controller controller, Shapes shape){
+	static void setShape(Controller controller, Shapes shape) {
 		if (controller.getActiveWorkspace() != null) {
+			
+			if (controller.getActiveWorkspace().getWorkspaceContent().getShape() != null &&
+							controller.getActiveWorkspace().getWorkspaceContent().getShape().shouldEventOnDrag) {
+				controller.getMouseDraftsmanEvents().stopUsingBrush();
+			}
+			
 			controller.getActiveWorkspace().getWorkspaceContent().setShape(shape);
+			
 			if (shape.shouldEventOnDrag) {
 				controller.getMouseDraftsmanEvents().startUsingBrush();
-			} else {
-				controller.getMouseDraftsmanEvents().stopUsingBrush();
 			}
 		}
 	}
