a764bb9dc85905f2dbc71efd60e793d8b5bfab4d
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-05-17 18:19:03 +0200

    add spaces in code

---
+++ src/sample/item/ItemConstructor.java
@@ -0,0 +1,54 @@
+package sample.item;
+
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+
+public interface ItemConstructor {
+	
+	
+	static void constructDoubleSpinner
+			(
+					Spinner<Double> spinner,
+					double min, double max, double step
+			)
+	{
+		SpinnerValueFactory.DoubleSpinnerValueFactory dsvf =
+				new SpinnerValueFactory.DoubleSpinnerValueFactory(
+						min, max
+				);
+		dsvf.setAmountToStepBy(
+				step
+		);
+		spinner.setValueFactory(
+				dsvf
+		);
+		spinner.getEditor().setOnAction(
+				actionEvent ->
+				{
+					try {
+						double newNumber = Double.parseDouble(
+								spinner.getEditor().getText()
+						);
+						if (newNumber > max)
+						{
+							spinner.getValueFactory().setValue(max);
+						}
+						else
+						{
+							spinner.getValueFactory().setValue(
+									Math.max(
+											newNumber, min
+									)
+							);
+						}
+					}
+					catch (NumberFormatException e)
+					{
+						spinner.getEditor().setText(
+								spinner.getValueFactory().getValue().toString()
+						);
+					}
+				});
+	}
+	
+}
