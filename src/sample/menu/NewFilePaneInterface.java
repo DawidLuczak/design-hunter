9510102278709f9af823ae9b2c1e79593e0c7cd7
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-05-17 18:26:57 +0200

    Create New File - validation methods

--- src/sample/menu/NewFilePaneInterface.java
+++ src/sample/menu/NewFilePaneInterface.java
@@ -4,25 +4,50 @@ import javafx.scene.control.TextField;
 
 public interface NewFilePaneInterface {
 
-	static boolean checkSizeInput(TextField field){
-		if (field.getText().isBlank()){
+	
+	static boolean checkSizeInput
+			(
+					TextField field
+			)
+	{
+		if (field.getText().isBlank())
+		{
 			return false;
-		} else {
-			try {
-				Integer.parseInt(field.getText());
-			} catch (NumberFormatException e) {
+		}
+		else
+		{
+			try
+			{
+				Integer.parseInt(
+						field.getText()
+				);
+			}
+			catch (NumberFormatException e)
+			{
 				return false;
 			}
 			return true;
 		}
 	}
 	
-	static void sizeInputLimiter(TextField field, int length){
-		field.textProperty().addListener((observableValue, s, t1) -> {
-			if (field.getText().length() > length) {
-				field.setText(field.getText().substring(0, length));
-			}
-		});
+	
+	static void sizeInputLimiter
+			(
+					TextField field, int length
+			)
+	{
+		field.textProperty().addListener(
+				(observableValue, s, t1) ->
+				{
+					if (field.getText().length() > length)
+					{
+						field.setText(
+								field.getText().substring(
+										0, length
+								)
+						);
+					}
+				});
 	}
 
 }
