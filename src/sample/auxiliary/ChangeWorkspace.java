09f4f164614c366612d1c585a5887e9ef989d0f8
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-05-17 18:16:34 +0200

    Add spaces in code

--- src/sample/auxiliary/ChangeWorkspace.java
+++ src/sample/auxiliary/ChangeWorkspace.java
@@ -4,12 +4,31 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import sample.Controller;
 
-public record ChangeWorkspace(Controller controller) implements ChangeListener<Number> {
+public record ChangeWorkspace
+		(
+				Controller controller
+		)
+		implements ChangeListener<Number>
+{
 	
-	@Override
-	public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-		controller.setActualWorkspace(controller.getWorkspaceList().get(t1.intValue()));
-		ControllerInterface.updateLayers(controller);
-		controller.setWorkspaceToolsProperties(controller.getActualWorkspace());
+		@Override
+	public void changed
+			(
+					ObservableValue<? extends Number> observableValue,
+					Number number,
+					Number t1
+			)
+	{
+		controller.setActualWorkspace(
+				controller.getWorkspaceList().get(
+						t1.intValue()
+				)
+		);
+		ControllerInterface.updateLayers(
+				controller
+		);
+		controller.setWorkspaceToolsProperties(
+				controller.getActualWorkspace()
+		);
 	}
 }
