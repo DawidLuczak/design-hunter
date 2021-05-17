a70ba56c647a21ad923675db71f12b43c1283b03
Dawid Luczak <dawid.luczak.it@gmail.com>
2021-05-17 17:26:07 +0200

    Mouse Event object setter deleted from record

--- src/sample/auxiliary/ChangeWorkspace.java
+++ src/sample/auxiliary/ChangeWorkspace.java
@@ -1,16 +1,15 @@
-package sample;
+package sample.auxiliary;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import sample.workspace.MouseDraftsmanInterface;
+import sample.Controller;
 
 public record ChangeWorkspace(Controller controller) implements ChangeListener<Number> {
 	
 	@Override
 	public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-		controller.setActiveWorkspace(controller.getWorkspaceList().get(t1.intValue()));
-		MouseDraftsmanInterface.setMouseDrawing(controller);
+		controller.setActualWorkspace(controller.getWorkspaceList().get(t1.intValue()));
 		ControllerInterface.updateLayers(controller);
-		controller.setWorkspaceToolsProperties(controller.getActiveWorkspace());
+		controller.setWorkspaceToolsProperties(controller.getActualWorkspace());
 	}
 }
